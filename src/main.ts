a7259d1fd79c4a9553b6496d3317e3c8071b4377
relato <relato.consultoria@gmail.com>
2019-11-09 12:16:24 -0300

    Cors and global prefix added to api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // angular
+      'http://localhost:3000', // react
+      'http://localhost:8081', // react-native
+    ],
+  });
   await app.listen(process.env.PORT || 3000);
 }
 
